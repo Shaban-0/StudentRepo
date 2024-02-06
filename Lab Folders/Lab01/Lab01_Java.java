b786d700ccf80919514ac317de3bf8d0ff1d61c2
Shaban Allarakha <shaban.allarakha@outlook.com>
2024-02-06 01:46:46 +0000

    Edited Lab01_Java in Gitpod

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -5,6 +5,7 @@ void testMethod1()
   //First command x = y + z
   //Second commnad print(x)
   // Edit made by Shaban Allarakha - 220210069
+  // Edit made by Shaban Allarakha - 220210069 on Gitpod
 
 
   //Last line print('Good bye')
